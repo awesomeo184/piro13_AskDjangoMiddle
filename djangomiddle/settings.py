c1a213be5b38d41d58268b5fd3adb9d3fc6fda59
awesomeo184 <awesomeo184@gmail.com>
2020-07-26 22:04:43 +0900

    4강

--- djangomiddle/settings.py
+++ djangomiddle/settings.py
@@ -110,3 +110,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+from django.urls import reverse_lazy
+
+LOGIN_URL = reverse_lazy('login')
+LOGOUT_REDIRECT_URL = reverse_lazy('login')
