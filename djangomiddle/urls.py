ea15e65d82ae2e8eea68548d01f7f3ef63747c2e
awesomeo184 <awesomeo184@gmail.com>
2020-07-26 21:45:34 +0900

    3강

--- djangomiddle/urls.py
+++ djangomiddle/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('accounts/', include('accounts.urls')),
-    path('', lambda req: redirect('/blog/')),
+    path('', lambda req: redirect('/blog/'), name='root'),
 ]
