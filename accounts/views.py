28c08f657033d3167647d733ea7ecc21f98ad552
awesomeo184 <awesomeo184@gmail.com>
2020-07-27 22:35:34 +0900

    7강

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
 from django.conf import settings
+from django.contrib.auth import login as auth_login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-# from django.views.generic import CreateView
-# from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, resolve_url
+from .forms import SignupForm
 
 
 @login_required()
@@ -13,12 +15,28 @@ def profile(request):
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
-            return redirect(settings.LOGIN_URL)
+            auth_login(request, user)
+            return redirect('profile')
     else:
-        form = UserCreationForm()
+        form = SignupForm()
     return render(request, 'accounts/signup.html', {'form': form})
 
-# signup = CreateView.as_view(model=User, form_class=UserCreationForm, success_url=settings.LOGIN_URL, template_name='accounts/signup.html')
+
+# class SignupView(CreateView):
+#     model = User
+#     form_class = SignupForm
+#     template_name = 'accounts/signup.html'
+#
+#     def get_success_url(self):
+#         return resolve_url('profile')
+#
+#     def form_valid(self, form):
+#         user = form.save()
+#         auth_login(self.request, user)
+#         return redirect(self.get_success_url())
+#
+#
+# signup = SignupView.as_view()
