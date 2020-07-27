127836839962a4f36210bb3b2ab018fdeb0314a5
awesomeo184 <awesomeo184@gmail.com>
2020-07-27 21:28:23 +0900

    5강

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,24 @@
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+# from django.views.generic import CreateView
+# from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+
 
 @login_required()
 def profile(request):
     return render(request, 'accounts/profile.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect(settings.LOGIN_URL)
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form': form})
+
+# signup = CreateView.as_view(model=User, form_class=UserCreationForm, success_url=settings.LOGIN_URL, template_name='accounts/signup.html')
